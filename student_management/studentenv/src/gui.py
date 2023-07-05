767be8d18658aae33e61e4874f2d235c4110268c
KarlCrisostomo12 <101928489+KarlCrisostomo12@users.noreply.github.com>
2023-07-05 18:47:40 +0800

    add

--- student_management/studentenv/src/gui.py
+++ student_management/studentenv/src/gui.py
@@ -2,5 +2,13 @@ import customtkinter as ctk
 
 window = ctk.CTk()
 
+window.geometry('500x500')
+
+
+def quit_btn():
+     window.destroy()
+     print('fuck you')
+
+button = ctk.CTkButton(window, command=quit_btn, text='Quit').pack()
 
 window.mainloop()
